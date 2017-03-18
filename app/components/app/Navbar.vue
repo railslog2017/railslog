bf041ccdf65ee0b6603d9d3ce1f375a8c935496e
GimYoungPhil <youngphil.gim@gmail.com>
2017-03-19 03:14:53 +0900

    fixed nav

--- app/components/app/Navbar.vue
+++ app/components/app/Navbar.vue
@@ -1,14 +1,16 @@
 <template lang="html">
-  <nav class="navbar navbar-toggleable-md navbar-light">
-    <div class="container">
-      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+  <div class="container">
+    <nav class="navbar navbar-toggleable-md navbar-light px-0">
+      <button class="navbar-toggler navbar-toggler-right" type="button"
+          data-toggle="collapse" data-target="#navbarTogglerDemo02"
+          aria-controls="navbarTogglerDemo02" aria-expanded="false"
+          aria-label="Toggle navigation" v-on:click="toggleNav">
         <span class="navbar-toggler-icon"></span>
       </button>
       <router-link to="/" class="navbar-brand">
-        Rails Log
+        Tiny Rails Log
       </router-link>
-
-      <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+      <div class="collapse navbar-collapse" v-bind:class="{ 'show': show }" id="navbarTogglerDemo02">
         <ul class="navbar-nav mr-auto mt-2 mt-md-0">
           <router-link tag="li" to="/relations" class="nav-item" active-class="active">
             <a class="nav-link">Relations</a>
@@ -21,12 +23,24 @@
           </router-link>
         </ul>
       </div>
-    </div>
-  </nav>
+    </nav>
+  </div>
 </template>
 
 <script>
 export default {
+
+  data() {
+    return {
+      show: false
+    }
+  },
+
+  methods: {
+    toggleNav() {
+      this.show = !this.show
+    }
+  }
 }
 </script>
 
